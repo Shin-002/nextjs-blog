688a6544763a8c8ed68a95fff46ea6ec7aa5d07c
Nick <shinichiro.yukawa@shelfy.jp>
2023-06-18 19:00:57 +0900

    ほぼ終わり

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from "../../components/layout/layout";
+import {getAllPostIds, getPostData} from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/Date";
+import utilStyle from "../../styles/utils.module.css";
+
+export async function getStaticProps({params}) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+export default function Post({postData}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingXl}>{postData.title}</h1>
+        <div className={utilStyle.lightText}>
+          <Date dateString={postData.date}/>
+        </div>
+        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+      </article>
+    </Layout>
+  )
+}
