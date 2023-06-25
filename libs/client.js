30630a86ab4a22ddb39151cbbec8cb9aa9b85988
Nick <shinichiro.yukawa@shelfy.jp>
2023-06-25 18:38:12 +0900

    環境変数追加

--- libs/client.js
+++ libs/client.js
@@ -2,6 +2,6 @@ import {createClient} from 'microcms-js-sdk'
 import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
 export const client = createClient({
-  serviceDomain: 'yuyu-blog',
-  apiKey: process.env.API_KEY
+  serviceDomain: process.env.MICRO_CMS_ENDPOINT,
+  apiKey: process.env.MICRO_CMS_API_KEY
 })
